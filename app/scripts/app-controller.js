71f3a00e9030bb65c6c7e3dc8f3ecab9a0ee4b12
we3x <veljko.simic98@gmail.com>
2015-11-28 14:46:53 +0100

    Get information about me

--- app/scripts/app-controller.js
+++ app/scripts/app-controller.js
@@ -2,7 +2,12 @@
 
 module.exports = [
   '$scope',
-  function schoolPlatformConfig($scope){
-    $scope.hello = "Helloooo :D"
+  '$http',
+  function schoolPlatformConfig($scope, $http){
+    $http.get('http://192.168.33.3:8000/api/v1/me/').then(function(data){
+      $scope.user = data[0];
+    })
+    console.log($scope.user)
+    $scope.hello = "Helloooo :D " + $scope.user;
   }
 ];
