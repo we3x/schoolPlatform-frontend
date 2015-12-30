9ffbb350841a024ace663bcaba607818e785c634
we3x <veljko.simic98@gmail.com>
2015-12-30 14:49:57 +0100

    Render text as html

--- app/scripts/student/tController.js
+++ app/scripts/student/tController.js
@@ -3,11 +3,13 @@
 module.exports = [
   '$scope',
   'texts',
-  function tController($scope, texts){
+  '$sce',
+  function tController($scope, texts, $sce){
     $scope.texts = texts.data;
-    $scope.current = $scope.texts[0];
     $scope.getCurrent = function(text){
       $scope.current = text;
+      $scope.ctext = $sce.trustAsHtml($scope.current.text)
     }
+    $scope.current = $scope.getCurrent($scope.texts[0])
   }
 ];
