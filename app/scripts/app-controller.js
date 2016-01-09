894dcc99029a08074bf2e3b2552ef3969e1cc0bb
we3x <veljko.simic98@gmail.com>
2016-01-09 14:33:00 +0100

    Insert userid

--- app/scripts/app-controller.js
+++ app/scripts/app-controller.js
@@ -10,7 +10,7 @@ module.exports = [
     $http.get(API_URL + 'me/').then(function(result){
       $scope.user = result.data[0];
       $window.sessionStorage.me = $scope.user.need;
-      $window.sessionStorage.id = $scope.user.uid;
+      $window.sessionStorage.id = $scope.user.id;
       $window.sessionStorage.username = $scope.user.username;
     }).then(function(){
       if ($scope.user.type == 'professor')
